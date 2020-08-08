bfda3276ec4b92ce850d69725519c35098ef9720
KVeitch <kirk.a.veitch@gmail.com>
2020-08-08 13:17:42 -0600

    Remove unwanted quotes in return

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const WrongLetters = () => {
-    return (
-        <div className="wrong-letters-container">
-            <div id="wrong-letters"></div>
-        </div>
-    )
-}
+const WrongLetters = ({ wrongLetters }) => {
+  return (
+    <div className="wrong-letters-container">
+      <div>
+        {wrongLetters.length > 0 && <p>Wrong</p>}
+        {wrongLetters
+          .map((letter, i) => <span key={i + letter}>{letter}</span>)
+          .reduce(
+            (prev, current) =>
+              prev === null ? [current] : [prev, ", ", current],
+            null
+          )}
+      </div>
+    </div>
+  );
+};
 
-export default WrongLetters
+export default WrongLetters;
