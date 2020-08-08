a78d98cb58c3a40ba5513ff3ad0acfc3cfef429e
KVeitch <kirk.a.veitch@gmail.com>
2020-08-08 15:39:01 -0600

    Add helper function to check win condition

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,6 +1,22 @@
 export function showNotification(setter) {
-    setter(true)
-    setTimeout(()=>{
-        setter(false)
-    },2000)
+  setter(true);
+  setTimeout(() => {
+    setter(false);
+  }, 2000);
+}
+
+export function checkWin(correctLetters, wrongLetters, word) {
+  let status = "win";
+  word.split("").forEach((letter) => {
+    if (!correctLetters.includes(letter)) {
+      status = "";
+    }
+  });
+
+  // Check for Loss
+  if (wrongLetters.length === 6) {
+    status = "lose";
+  }
+
+  return status;
 }
