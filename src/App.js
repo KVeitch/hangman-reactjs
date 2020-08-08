886aefc6ee8a832fc42f069673c19a4c7f6dc97d
KVeitch <kirk.a.veitch@gmail.com>
2020-08-08 11:26:23 -0600

    Create and link WrongLetters component to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure';
+import WrongLetters  from './components/WrongLetters';
 
 function App() {
   return (
     <>
       <Header />
+      <Figure />
+      <WrongLetters />
+
     </>
   );
 }
