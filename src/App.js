bf8aacee7cfc53301f632a145956e53e654dab5a
KVeitch <kirk.a.veitch@gmail.com>
2020-08-08 12:50:17 -0600

    Add functionality to display word letters and blanks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Figure from './components/Figure';
-import WrongLetters  from './components/WrongLetters';
-import Word  from './components/Word';
-import Popup  from './components/Popup';
-import Notification  from './components/Notification';
+import React, {useState} from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Popup from "./components/Popup";
+import Notification from "./components/Notification";
+import words from './components/wordList'
+
+
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters]  = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <>
       <Header />
-      <div className='game-container'>
+      <div className="game-container">
         <Figure />
         <WrongLetters />
-        <Word />
+      <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
       </div>
-
-
     </>
   );
 }
