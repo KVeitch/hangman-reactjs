affae0ef70745522f665c9629a45bb05e8be3b43
KVeitch <kirk.a.veitch@gmail.com>
2020-08-08 13:30:27 -0600

    Add Figure display with wrong guess

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Figure from "./components/Figure";
@@ -16,17 +16,45 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters]  = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      const { key, keyCode } = e;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(correctLetters=>[...correctLetters, letter]);
+
+           } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters=>[...wrongLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    };
+    window.addEventListener('keydown',handleKeydown)
+    return () => window.removeEventListener('keydown',handleKeydown)
+  },[correctLetters, wrongLetters, playable]);
+
+
 
   return (
     <>
       <Header />
       <div className="game-container">
-        <Figure />
-        <WrongLetters />
-      <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+        <Figure wrongLetters={wrongLetters}/>
+        <WrongLetters wrongLetters={wrongLetters}/>
+        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
       </div>
     </>
   );
 }
 
 export default App;
+
